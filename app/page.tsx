983cc0b5e207e41ef5aa85fe5504695d1f30368f
Elaina Nguyen <nguyenelaina@gmail.com>
2023-07-20 00:42:13 -0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+/* export const dynamic = "force-dynamic" */
+
 import Container from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";
