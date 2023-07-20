b0491b014ada288910357de75a334e3eb8863aee
Elaina Nguyen <nguyenelaina@gmail.com>
2023-07-20 11:09:58 -0500

    added force-dynamic to fix server side rendering issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-/* export const dynamic = "force-dynamic" */
+export const dynamic = "force-dynamic";
 
 import Container from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
