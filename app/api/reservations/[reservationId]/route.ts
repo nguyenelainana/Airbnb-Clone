2b75962dd00a5a948c29993d4e063affd7fa0ee8
Elaina Nguyen <nguyenelaina@gmail.com>
2023-07-20 00:04:55 -0500

    fixed

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -4,7 +4,7 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
 
 interface IParams {
-  reversationId?: string;
+  reservationId?: string;
 }
 
 export async function DELETE(
